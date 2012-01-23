0a1601d4267fe0d4d581b8cd0449645fd122e54e
Luke Chadwick <luke.a.chadwick@gmail.com>
2012-01-24 08:50:23 +1100

    Start working on a single page for updating the domain details

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,3 +1,7 @@
-Then /^I should be on the (.+)$/ do |page_name|
+Then /^I should be on the manage domain page for '(.+)'$/ do |domain_name|
+  current_path.should =~ /#{Domain.where(:name => domain_name).first.id}$/
+end
+
+Then /^I should be on the (.+ page)$/ do |page_name|
   current_path.should == path_to(page_name)
 end
