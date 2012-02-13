e813e25df36764ecb946eca1b730992a8dedfec3
Luke Chadwick <luke.a.chadwick@gmail.com>
2012-02-14 10:02:03 +1100

    Passing back results from ajax form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,9 +6,23 @@
 
 $(document).ready(function(){
   /* Your javascripts goes here... */
-  $(document).on('ajax:error', function(data) {
-    alert("Error");
-    alert(data)
+  
+  $('#new-mapping form').bind("ajax:before", function() {
+    console.log("ajax:before");
+  });
+
+  $('#new-mapping form').bind("ajax:success", function(data, status, xhr) {
+    console.log("ajax:success");
+    console.log(data)
+    console.log(xhr.responseText)
+  });
+
+  $('#new-mapping form').bind("ajax:complete", function() {
+    console.log("ajax:complete");
+  });
+  $('#new-mapping form').bind("ajax:error", function(xhr, status, error) {
+      console.log("ajax:error");
+      console.log(error);
   });
 });
   
